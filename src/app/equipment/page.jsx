8984425b3f6fa17fc7ea4c932b151c421ff39003
intaaudio <cs@inta-audio.com>
2024-10-01 14:18:24 +0100

    Amended equipment page as it was failing deployment.

---
+++ src/app/equipment/page.jsx
@@ -0,0 +1,25 @@
+//Hooks & Plugins
+import Link from "next/link";
+import Image from "next/image";
+
+//Components
+import Header from "@/app/components/Header";
+
+export default function equipment() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h2
+          className={`my-2 px-mobileXPadding text-2xl font-semibold text-blueBlack dark:text-almostWhite`}
+        >
+          GALLERY
+        </h2>
+        <Link href={"/astrophotography"}>
+          <Image src={"#"} alt={"#"}></Image>
+          <h3>ASTROPHOTOGRAPHY</h3>
+        </Link>
+      </main>
+    </>
+  );
+}
