e82158fb183c8cf922bc1a1d0868fb513efd7de9
intaaudio <cs@inta-audio.com>
2024-10-22 17:28:44 +0100

    Added a filter functionality for the searchbar. While testing, I have hard-coded the values into the water page.
    Need to apply this dynamically to other pages.

--- src/app/components/PageTemplate.jsx
+++ src/app/components/PageTemplate.jsx
@@ -15,6 +15,8 @@ export function PageTemplate({
   pageTitle,
 }) {
   const [searchText, setSearchText] = useState("");
+  const [radioBtnValue, setRadioBtnValue] = useState("");
+  const [showFilterMenu, setShowFilterMenu] = useState(false);
 
   const handleSearchTerm = (e) => {
     const searchTerm = e.target.value;
@@ -41,6 +43,18 @@ export function PageTemplate({
     }
   });
 
+  const handleRadioButtonValue = (e) => {
+    setRadioBtnValue(e.target.value);
+  };
+
+  const handleShowFilterMenu = () => {
+    setShowFilterMenu(!showFilterMenu);
+  };
+
+  useEffect(() => {
+    setSearchText(radioBtnValue);
+  }, [radioBtnValue]);
+
   return (
     <section
       // style={{ minHeight: `calc(100vh - ${headerHeight}px)` }}
@@ -57,15 +71,62 @@ export function PageTemplate({
         </p>
 
         {!isLocationCategory && (
-          <input
-            placeholder={`${
-              isAstroImage
-                ? "Search by name or catalogue ID"
-                : "Search by name or location"
-            }`}
-            className={`mb-4 h-8 w-full rounded bg-black/10 px-2 xl:h-10 xl:text-xl dark:bg-white/10 dark:text-almostWhite`}
-            onChange={handleSearchTerm}
-          ></input>
+          <div className={`relative mb-4 flex items-center gap-2`}>
+            <input
+              aria-label={"Search bar to filter images"}
+              placeholder={`${
+                isAstroImage
+                  ? "Search by name or catalogue ID"
+                  : "Search by name or location"
+              }`}
+              className={`h-8 w-full rounded bg-black/10 px-2 xl:h-10 xl:text-xl dark:bg-white/10 dark:text-almostWhite`}
+              onChange={handleSearchTerm}
+              value={searchText}
+            ></input>
+            <button
+              onClick={handleShowFilterMenu}
+              className={`flex h-8 w-8 items-center justify-center rounded xl:h-10 xl:w-10 dark:bg-white/10 dark:hover:bg-white/15`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="#e8eaed"
+                viewBox="0 -960 960 960"
+              >
+                <path d="M400-240v-80h160v80H400zM240-440v-80h480v80H240zM120-640v-80h720v80H120z"></path>
+              </svg>
+            </button>
+            <fieldset
+              className={`absolute ${showFilterMenu ? "block" : "hidden"} right-0 top-10 z-20 flex flex-col gap-2 border-[1px] border-white/40 bg-almostWhite dark:bg-almostBlack`}
+            >
+              <legend className="text-almostWhite">Filter by location</legend>
+              <div>
+                <input
+                  onClick={(e) => handleRadioButtonValue(e)}
+                  type={"radio"}
+                  name={"filter"}
+                  id={"iceland"}
+                  value={"iceland"}
+                />
+                <label className={`text-almostWhite`} htmlFor={"iceland"}>
+                  iceland
+                </label>
+              </div>
+              <div>
+                <input
+                  onClick={(e) => handleRadioButtonValue(e)}
+                  type={"radio"}
+                  name={"filter"}
+                  id={"dartmoor"}
+                  value={"dartmoor"}
+                />
+                <label className={`text-almostWhite`} htmlFor={"dartmoor"}>
+                  dartmoor
+                </label>
+              </div>
+            </fieldset>
+          </div>
         )}
         <Gallery images={filteredImages} isAstroImage={isAstroImage} />
       </section>
